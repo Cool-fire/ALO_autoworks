b277f14a944dbde0cbe1c3b7a9792f15162ee44a
Coo-fire <upendrareddy2511@gmail.com>
2018-05-31 21:46:58 +0530

    signup dsign

--- app/src/main/java/com/aloautoworks/alo/signup.java
+++ app/src/main/java/com/aloautoworks/alo/signup.java
@@ -2,12 +2,22 @@ package com.aloautoworks.alo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class signup extends Activity {
+public class signup extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("signup");
+        myToolbar.setTitleTextColor(getResources().getColor(R.color.colorwhite));
+
+
+
     }
 }
